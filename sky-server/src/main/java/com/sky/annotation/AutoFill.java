1ed3a86d2585d759bc6fbac2457f4ea89667c15a
zzhlaptop <706561344@qq.com>
2026-04-29 19:04:45 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/annotation/AutoFill.java
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -10,6 +10,7 @@ import java.lang.annotation.Target;
 /**
  * 自定义注解，用于标识某个方法需要进行功能字段自动填充处理
  */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AutoFill {
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -80,11 +80,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         //密码md5加密
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        employee.setCreateTime(LocalDateTime.now());
+/*        employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         //TODO 后期需要改为当前登陆用户的id
         employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());*/
 
         employeeMapper.insert(employee);
 
@@ -137,8 +137,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO,employee);
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+/*        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());*/
         employeeMapper.update(employee);
     }
 

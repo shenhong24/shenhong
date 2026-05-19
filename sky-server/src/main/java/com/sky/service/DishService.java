e69891ad05f4d3150cf8b5431775973f1ad9dfaf
shenhong <706561344@qq.com>
2026-05-19 17:46:03 +0800

    商品浏览业务功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -49,5 +49,12 @@ public interface DishService {
      */
     List<Dish> list(Long categoryId);
 
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
+
 
 }
